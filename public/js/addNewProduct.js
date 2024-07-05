ee9daa8ae2dd570389d310739e2000dd23f4636e
bárbara miranda <barbaramntmiranda@gmail.com>
2024-07-05 14:09:21 -0300

    handling submit errors

--- public/js/addNewProduct.js
+++ public/js/addNewProduct.js
@@ -1,5 +1,7 @@
 import { connectAPI } from "./connectApi.js";
+const form = document.querySelector("[data-form]");
 const btnAdd = document.getElementById("btn_adiciona");
+const messageErro = document.querySelector(".message_error");
 
 async function addNewProduct(event) {
     event.preventDefault();
@@ -7,6 +9,8 @@ async function addNewProduct(event) {
     const name = document.querySelector("[data-name]").value;
     const price = document.querySelector("[data-price]").value;
     const image = document.querySelector("[data-image]").value;
+
+    btnAdd.setAttribute("disabled", "disabled");
 
     try {
         await connectAPI.addNewItem(name, price, image);
@@ -16,11 +20,13 @@ async function addNewProduct(event) {
 
         setTimeout(() => {
             btnAdd.textContent = "Adicionar";
+            btnAdd.removeAttribute("disabled");
         }, 2000);
 
     } catch (error) {
-        console.log(error);
+        console.log("Erro ao adicionar produto:", error);
+        messageErro.innerHTML = "O produto não foi adicionado. Tente novamente."
     }
 }
 
-btnAdd.addEventListener("click", event => addNewProduct(event))
+form.addEventListener("submit", event => addNewProduct(event));
